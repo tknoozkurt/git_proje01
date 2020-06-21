5e597b6b5f3ea81179c6ba62f908072627ad9a3e
Tekin <tekinozkurt@tekins-MacBook-Air.local>
2020-06-21 15:43:10 -0400

    ilk commit

--- src/package01/FirstClass01.java
+++ src/package01/FirstClass01.java
@@ -5,6 +5,10 @@ public class FirstClass01 {
 	public static void main(String[] args) {
      System.out.println("Ilk commit from local repository");
      System.out.println("Ikinci commit from local repository");
+     
+     System.out.println("feature olusturdum");
+     System.out.println("yeni branchten commit denemesi ");
+
 	}
 
 }
